b112c02a643ca251c22ada29e47a231d6a54307b
raluca.toth@8x8.com <raluca.toth@8x8.com>
2020-07-31 15:28:34 +0300

    Store a window of Jibri availability metric and scale based on it

--- src/autoscaler.ts
+++ src/autoscaler.ts
@@ -9,53 +9,100 @@ export interface AutoscaleProcessorOptions {
     jibriGroupList: Array<string>;
     jibriMinDesired: number;
     jibriMaxDesired: number;
+    jibriScaleUpThreshold: number;
+    jibriScaleDownThreshold: number;
+    jibriScalePeriod: number;
+    jibriScaleUpPeriodsCount: number;
+    jibriScaleDownPeriodsCount: number;
 }
 
 export default class AutoscaleProcessor {
     private jibriTracker: JibriTracker;
     private jibriMinDesired: number;
     private jibriMaxDesired: number;
+    private jibriScaleUpThreshold: number;
+    private jibriScaleDownThreshold: number;
     private jibriGroupList: Array<string>;
+    private jibriScalePeriod: number;
+    private jibriScaleUpPeriodsCount: number;
+    private jibriScaleDownPeriodsCount: number;
 
     constructor(options: AutoscaleProcessorOptions) {
         this.jibriTracker = options.jibriTracker;
         this.jibriMinDesired = options.jibriMinDesired;
         this.jibriMaxDesired = options.jibriMaxDesired;
+        this.jibriScaleUpThreshold = options.jibriScaleUpThreshold;
+        this.jibriScaleDownThreshold = options.jibriScaleDownThreshold;
         this.jibriGroupList = options.jibriGroupList;
+        this.jibriScalePeriod = options.jibriScalePeriod;
+        this.jibriScaleUpPeriodsCount = options.jibriScaleUpPeriodsCount;
+        this.jibriScaleDownPeriodsCount = options.jibriScaleDownPeriodsCount;
 
         this.processAutoscaling = this.processAutoscaling.bind(this);
         this.processAutoscalingByGroup = this.processAutoscalingByGroup.bind(this);
     }
     async processAutoscaling(): Promise<boolean> {
-        await Promise.all(this.jibriGroupList.map(this.processAutoscalingByGroup))
+        await Promise.all(this.jibriGroupList.map(this.processAutoscalingByGroup));
         return true;
     }
 
     async processAutoscalingByGroup(group: string): Promise<boolean> {
         const currentInventory = await this.jibriTracker.getCurrent(group);
         const count = currentInventory.length;
-        let idleCount = 0;
 
-        const minDesired = this.jibriMinDesired;
-        const maxDesired = this.jibriMaxDesired;
+        //TODO get both inventories from one redis call
+        const metricInventoryScaleUp = await this.jibriTracker.getMetricPeriods(
+            group,
+            this.jibriScaleUpPeriodsCount,
+            this.jibriScalePeriod,
+        );
+        const metricInventoryScaleDown = await this.jibriTracker.getMetricPeriods(
+            group,
+            this.jibriScaleDownPeriodsCount,
+            this.jibriScalePeriod,
+        );
 
-        logger.info('inventory', { group, currentInventory });
+        let computedMetricScaleUp = 0;
+        let computedMetricScaleDown = 0;
 
-        currentInventory.forEach(item => {
-            if (item.status.busyStatus == JibriStatusState.Idle) {
-                idleCount += 1;
-            }
+        metricInventoryScaleUp.forEach((item) => {
+            computedMetricScaleUp += item.value;
         });
+        computedMetricScaleUp = computedMetricScaleUp / this.jibriScaleUpPeriodsCount;
 
-        if (idleCount < maxDesired || count < minDesired) {
+        metricInventoryScaleDown.forEach((item) => {
+            computedMetricScaleDown += item.value;
+        });
+        computedMetricScaleDown = computedMetricScaleDown / this.jibriScaleDownPeriodsCount;
+
+        logger.info('===== Evaluating scale computed metrics....');
+
+        if (
+            (count < this.jibriMaxDesired && computedMetricScaleUp < this.jibriScaleUpThreshold) ||
+            count < this.jibriMinDesired
+        ) {
             // scale up here
-            logger.info('Group should scale up, idle count not high enough', { group, idleCount, minDesired });
+            logger.info('Group should scale up, idle count not high enough', {
+                group,
+                computedMetricScaleUp,
+                count,
+            });
+        } else if (count > this.jibriMinDesired && computedMetricScaleDown > this.jibriScaleDownThreshold) {
+            // scale down here
+            logger.info('Should scale down here, idle count too high', {
+                group,
+                computedMetricScaleDown,
+                count,
+            });
+        } else {
+            logger.info('NOTHING TO BE DONE. ', {
+                group,
+                computedMetricScaleUp,
+                computedMetricScaleDown,
+                count,
+            });
         }
 
-        if (idleCount > maxDesired) {
-            // scale down here
-            logger.info('Should scale down here, idle count too high', { group, idleCount, maxDesired })
-        }
         return true;
     }
 }
--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ export default {
     RedisPort: process.env.REDIS_PORT || 6379,
     RedisPassword: process.env.REDIS_PASSWORD,
     ProtectedApi: process.env.PROTECTED_API || true,
-    AsapPubKeyTTL: process.env.ASAP_PUB_KEY_TTL || 3600,
+    AsapPubKeyTTL: Number(process.env.ASAP_PUB_KEY_TTL || 3600),
     AsapPubKeyBaseUrl: process.env.ASAP_PUB_KEY_BASE_URL,
     AsapJwtAcceptedAud: process.env.ASAP_JWT_AUD,
     AsapJwtAcceptedHookIss: process.env.ASAP_JWT_ACCEPTED_HOOK_ISS,
@@ -18,4 +18,12 @@ export default {
     JibriMinDesired: Number(process.env.JIBRI_MIN_DESIRED || 1),
     JibriMaxDesired: Number(process.env.JIBRI_MAX_DESIRED || 1),
     JibriGroupList: (process.env.JIBRI_GROUP_LIST || 'default').split(' '),
+    // Scale out if less than 1 jibris is available for 2 periods of 60 seconds
+    JibriScaleUpThreshold: Number(process.env.JIBRI_SCALE_UP_THRESHOLD || 1),
+    // Scale in if more than 2 jibris are available for 10 periods of 60 seconds
+    JibriScaleDownThreshold: Number(process.env.JIBRI_SCALE_DOWN_THRESHOLD || 2),
+    // Jibris should send roughly every 60 seconds a new status report
+    JibriScalePeriod: Number(process.env.JIBR_SCALE_PERIOD || 60),
+    JibriScaleUpPeriodsCount: Number(process.env.JIBRI_SCALE_UP_PERIODS_COUNT || 2),
+    JibriScaleDownPeriodsCount: Number(process.env.JIBRI_SCALE_DOWN_PERIODS_COUNT || 4),
 };

061dee389d725e328838ab8b4797f35c72d5e14f
Aaron van Meerten <aaron.van.meerten@gmail.com>
2020-08-20 11:48:01 -0500

    FEAT: add timestamp to logs

--- src/logger.ts
+++ src/logger.ts
@@ -2,6 +2,7 @@ import winston from 'winston';
 import config from './config';
 
 const options: winston.LoggerOptions = {
+    format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
     transports: [
         new winston.transports.Console({
             level: config.LogLevel,

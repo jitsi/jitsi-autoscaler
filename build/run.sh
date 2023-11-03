ddc150a2b8cbbe196b47e41b1bd424ac43c0b149
Aaron van Meerten <aaron.van.meerten@8x8.com>
2023-11-03 14:59:45 -0500

    updated run command

--- build/run.sh
+++ build/run.sh
@@ -8,4 +8,13 @@ if [ -f /usr/jitsi/pre-run.sh ]; then
     . /usr/jitsi/pre-run.sh
 fi
 
-exec node /usr/src/app/app.js
+if [ "$ENABLE_NODE_PERF" == "true" ]; then
+    NODE_CMD_PREFIX="perf record -e cycles:u -g --"
+    NODE_PERF_PARAM="--perf-basic-prof"
+fi
+
+if [ "$ENABLE_PROFILING" == "true" ]; then
+    NODE_PERF_PARAM="--prof"
+fi
+
+exec $NODE_CMD_PREFIX node $NODE_PERF_PARAM /usr/src/app/app.js

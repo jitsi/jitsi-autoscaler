661ffcac00c9b85b5e613035d67deed10a8bb2e1
Aaron van Meerten <aaron.van.meerten@8x8.com>
2023-11-27 13:52:10 -0600

    feature: add git tag on docker build

--- build/build.sh
+++ build/build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-# last tag 0.0.1
+# last tag 0.0.22
 docker buildx build --no-cache --platform=linux/arm64,linux/amd64 --push --pull --progress=plain --tag jitsi/autoscaler:$TAG --tag jitsi/autoscaler:latest .
+
+if [ $? -ne 0 ]; then
+    echo "Build failed"
+    exit 1
+fi
+
+git tag -a "docker-$TAG" -m "Published in dockerhub as jitsi/autoscaler:$TAG"
+git push origin "docker-$TAG"
